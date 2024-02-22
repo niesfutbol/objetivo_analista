259c0d4b7c4cadf9c000d9ff16b613c06fbb1e76
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-21 21:19:31 -0800

    🚧 Test differnce between xpoint

--- src/xP_barcelona.R
+++ src/xP_barcelona.R
@@ -19,3 +19,10 @@ real_madrid_id <- 541
 home_xP <- laliga |> get_home_xP(id_team = real_madrid_id)
 away_xP <- laliga |> get_away_xP(id_team = real_madrid_id)
 xpoint_real_madrid <- c(home_xP, away_xP)
+
+
+
+real_madrid <- to_vec(for(i in 1:2000) mean(sample(xpoint_real_madrid, replace = TRUE)))
+barca <- to_vec(for(i in 1:2000) mean(sample(xpoint_barca, replace = TRUE)))
+
+mean(barca - real_madrid > 0 )
