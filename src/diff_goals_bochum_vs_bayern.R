4ce07f344751e98ef6ddf56ced05905d2fe286dc
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-21 21:23:37 -0800

    🎨 Format with Styler

--- src/diff_goals_bochum_vs_bayern.R
+++ src/diff_goals_bochum_vs_bayern.R
@@ -1,6 +1,6 @@
 diff_gol <- rpois(2000, 2.65) - rpois(2000, 1.64)
-xp_Bayer <- mean(diff_gol > 0)*3 + mean(diff_gol == 0)
-xp_Bochum <- mean(diff_gol < 0)*3 + mean(diff_gol == 0)
+xp_Bayer <- mean(diff_gol > 0) * 3 + mean(diff_gol == 0)
+xp_Bochum <- mean(diff_gol < 0) * 3 + mean(diff_gol == 0)
 
 xg_bochum_nies <- 1.9446820000000002
 xg_bayer_nies <- 2.471421
--- src/xP_barcelona.R
+++ src/xP_barcelona.R
@@ -3,11 +3,15 @@ library(tidyverse)
 laliga <- read_csv("tests/data/league_140_2023.csv", show_col_types = FALSE)
 
 get_away_xP <- function(league, id_team) {
-  league |> filter(away_id == id_team) %>% .$away_xPoints
+  league |>
+    filter(away_id == id_team) %>%
+    .$away_xPoints
 }
 
 get_home_xP <- function(league, id_team) {
-  league |> filter(home_id == id_team) %>% .$home_xPoints
+  league |>
+    filter(home_id == id_team) %>%
+    .$home_xPoints
 }
 
 barcelona_id <- 529
@@ -22,7 +26,7 @@ xpoint_real_madrid <- c(home_xP, away_xP)
 
 
 
-real_madrid <- to_vec(for(i in 1:2000) mean(sample(xpoint_real_madrid, replace = TRUE)))
-barca <- to_vec(for(i in 1:2000) mean(sample(xpoint_barca, replace = TRUE)))
+real_madrid <- to_vec(for (i in 1:2000) mean(sample(xpoint_real_madrid, replace = TRUE)))
+barca <- to_vec(for (i in 1:2000) mean(sample(xpoint_barca, replace = TRUE)))
 
-mean(barca - real_madrid > 0 )
+mean(barca - real_madrid > 0)
